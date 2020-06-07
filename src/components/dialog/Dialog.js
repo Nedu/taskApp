9a4463882da9537508583850e40b3e253748e536
Nedu Robert <nedumrobert@gmail.com>
2020-06-07 01:55:44 -0400

    feat: Dialog

---
+++ src/components/dialog/Dialog.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+    Button, 
+    Dialog, 
+    DialogActions, 
+    DialogContent,
+    DialogContentText,
+    DialogTitle 
+} from '@material-ui/core';
+
+export const CustomDialog = ({
+    isOpen,
+    handleClose,
+    title,
+    subtitle,
+    children
+}) => {
+    return (
+        <>
+            <Dialog fullWidth maxWidth='md' open={isOpen} onClose={handleClose} aria-labelledby='max-width-dialog-title'>
+                <DialogTitle id='max-width-dialog-title'>{title}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>{subtitle}</DialogContentText>
+                    {children}
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color='primary'>Close</Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    )
+}
